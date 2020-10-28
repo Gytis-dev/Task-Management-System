623a1da10c349274c7da532cc4957df8d76e2e88
Gytis-dev <68816480+Gytis-dev@users.noreply.github.com>
2020-10-28 20:26:54 +0200

    sksksksksksks

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -65,6 +65,7 @@ let changeStatus = (item) => {
 
     return (
         <Router>
+            belekas
             <div className="wrapper-home">
                 <Navbar name={useris} handleFormSubmit={submit} />
                 <LeftSection />
