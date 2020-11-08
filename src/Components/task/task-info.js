4143b5f0ae9f96740dbdadf1c132520a42bd2c6d
Mikolonija <bieliauskasmodestas47@gmail.com>
2020-11-08 16:31:55 +0200

    fix code

--- src/Components/task/task-info.js
+++ src/Components/task/task-info.js
@@ -41,47 +41,54 @@ const TaskInfo = (props) => {
 
   return (
     <div className="task-info g-center font-normal-font">
-      <div >
-      <div
-        className="task-info-back-button g-left  font-normal-font"
-        onClick={() => props.history.push("/home")}
-      >
-        Back
-      </div>
-      <div className="font-sami-big g-left task-info-title "> {name}</div>
-      <div className="font-normal-font g-left task-info-title ">
-        Reporter: {reporter}
-      </div>
-      <div className="font-normal-font g-left task-info-title ">
-        Priority: {priority}
-      </div>
-      <div className="font-normal-font g-left task-info-title ">
-        Date: {date}
-      </div>
-      <div className=" g-left">
-        <div className="font-normal-font  task-info-title-status" onClick={changeStatus}>
-          <div>
-        Status: {state.status}
+      <div>
+        <div
+          className="task-info-back-button g-left  font-normal-font"
+          onClick={() => props.history.push("/home")}
+        >
+          Back
         </div>
-        <div  className="task-info-title-status-edit">
-          <FontAwesomeIcon icon={faEdit} />
+        <div className="font-sami-big g-left task-info-title "> {name}</div>
+        <div className="font-normal-font g-left task-info-title ">
+          Reporter: {reporter}
         </div>
+        <div className="font-normal-font g-left task-info-title ">
+          Priority: {priority}
         </div>
-      </div>
-    
-        <div className="font-normal-font g-left task-info-title-comment ">Comment</div>
+        <div className="font-normal-font g-left task-info-title ">
+          Date: {date}
+        </div>
+        <div className=" g-left">
+          <div
+            className="font-normal-font  task-info-title-status"
+            onClick={changeStatus}
+          >
+            <div>Status: {state.status}</div>
+            <div className="task-info-title-status-edit">
+              <FontAwesomeIcon icon={faEdit} />
+            </div>
+          </div>
+        </div>
+        <div className="font-normal-font g-left task-info-title-comment ">
+          Comment
+        </div>
         <div className="g-left task-info-comment ">
-      <textarea className="task-info-comment-style" type="text" value={val} onChange={(e) => setVal(e.target.value)} />
-      <button className="g-btn-actyve " onClick={addComment}>Add comment</button>
-  
+          <textarea
+            className="task-info-comment-style"
+            type="text"
+            value={val}
+            onChange={(e) => setVal(e.target.value)}
+          />
+          <button className="g-btn-actyve " onClick={addComment}>
+            Add comment
+          </button>
+        </div>
+        <div className="g-left task-info-comment-text">
+          {koment.map((k) => {
+            return <div key={k}> {k} </div>;
+          })}
+        </div>
       </div>
-      <div className="g-left task-info-comment-text">
-        {koment.map((k) => {
-          return <div key={k}> {k} </div>;
-        })}
-      </div>
-      </div>
-
     </div>
   );
 };
